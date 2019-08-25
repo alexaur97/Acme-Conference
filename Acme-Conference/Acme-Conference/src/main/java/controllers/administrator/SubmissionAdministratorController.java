98cc602eecc343eba5c778b66ac8053b91f2179b
Andrea <andreadommon12345@gmail.com>
2019-08-25 12:45:06 +0200

    Service, controller and repository

---
+++ Acme-Conference/Acme-Conference/src/main/java/controllers/administrator/SubmissionAdministratorController.java
@@ -0,0 +1,51 @@
+
+package controllers.administrator;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import services.AdministratorService;
+import services.SubmissionService;
+import controllers.AbstractController;
+import domain.Submission;
+
+@Controller
+@RequestMapping("submission/administrator")
+public class SubmissionAdministratorController extends AbstractController {
+
+	@Autowired
+	private SubmissionService		submissionService;
+
+	@Autowired
+	private AdministratorService	administratorService;
+
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ModelAndView list() {
+		ModelAndView result;
+		try {
+			this.administratorService.findByPrincipal();
+
+			final Collection<Submission> submissionsUnderReview = this.submissionService.findSubmissionUnderReview();
+			final Collection<Submission> submissionsAccepted = this.submissionService.findSubmissionAccepted();
+			final Collection<Submission> submissionsRejected = this.submissionService.findSubmissionRejected();
+
+			result = new ModelAndView("submission/listAdm");
+			result.addObject("requestURI", "/submission/administrator/list.do");
+			result.addObject("submissionsUnderReview", submissionsUnderReview);
+			result.addObject("submissionsAccepted", submissionsAccepted);
+			result.addObject("submissionsRejected", submissionsRejected);
+
+		} catch (final Exception e) {
+			result = new ModelAndView("redirect:/#");
+		}
+
+		return result;
+	}
+
+}
