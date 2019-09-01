3cda4575b503efc9e4743fa5417cf43c2158719d
alexaur97 <ugarte97aur@gmail.com>
2019-09-01 14:56:28 +0200

    report

--- Acme-Conference/Acme-Conference/src/main/java/services/ReportService.java
+++ Acme-Conference/Acme-Conference/src/main/java/services/ReportService.java
@@ -5,9 +5,11 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
 
 import domain.Author;
 import domain.Report;
+import domain.Reviewer;
 import domain.Submission;
 import repositories.AuthorRepository;
 import repositories.ReportRepository;
@@ -18,10 +20,17 @@ public class ReportService {
 
 	@Autowired
 	private ReportRepository reportRepository;
-	
+
 	@Autowired
 	private AuthorService authorService;
 	
+	@Autowired
+	private ReviewerService reviewerService;
+	
+	public Report create() {
+		return new Report();
+	}
+
 	public Collection<Report> findReportsBySubmission(Submission s) {
 		return this.reportRepository.findReportsBySubmission(s.getId());
 	}
@@ -41,6 +50,21 @@ public class ReportService {
 
 	public Report findOne(int reportId) {
 		return this.reportRepository.findOne(reportId);
+	}
+
+	public Report reconstruct(Report report, BindingResult binding) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Report save(Report report) {
+		return this.reportRepository.save(report);
+		
+	}
+
+	public Collection<Report> findReportsByPrincipal() {
+		Reviewer principal = this.reviewerService.findByPrincipal();
+		return this.reportRepository.findReportsByPrincipal(principal);
 	}
 
 }
--- README.md
+++ README.md
@@ -6,3 +6,15 @@ A tener en cuenta:
 actividades, etc. así como realizar decisiones en sus submissions y demás
 operaciones similares es la conferencia que se llama "British Aeroespace",
 que es la que tiene más relaciones en el archivo 'PopulateDataBase.xml'.
+
+2. Hemos considerado que una Activity solo puede crearse para una Conference cuyo
+estado sea 'FINAL', ya que de esa manera la Conference ya no puede editarse, evitando
+así incompatibilidades con el horario que abarque una actividad en el caso de que el
+horario de una conferencia pudiera cambiarse.
+
+3. A la hora de enviar mensajes difundidos, entendemos y hemos contado con que el 
+administrador, independientemente de cuántas registrations o submissions tenga una 
+conferencia, si desea notificar a X posibles usuarios de algo en concreto, pueda 
+seleccionar cualquier conferencia con estado FINAL que haya en la base de datos sin
+ningún tipo de restricción. En el caso de que dicha conferencia no tuviera ninguna 
+registration o submission, no pasaría nada y no se crearía ningún mensaje.
